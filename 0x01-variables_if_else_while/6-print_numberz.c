74650d95bddf934db2f686c9028a36a60efc4c23
sandovbarr <sandovbarr@gmail.com>
2020-02-06 22:14:33 +0000

    numbers6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - give a alphabetic in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
