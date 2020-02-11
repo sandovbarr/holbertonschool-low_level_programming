dce46bc55466a68cb79f0442dba2f8ef88ea99b3
sandovbarr <sandovbarr@gmail.com>
2020-02-11 15:22:45 +0000

    fix2exer

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,15 @@
 #include "holberton.h"
+
+/**
+ * print_alphabet_x10 - give a alphabetic in lowercase
+ * Return: 0
+ */
+
 void print_alphabet_x10(void)
 {
-        int a, b;
+	int a, b;
 
-	for (b = 0; b <= 10; b++)
+	for (b = 0; b <= 9; b++)
 	{
 		for (a = 'a'; a <= 'z'; a++)
 		{
