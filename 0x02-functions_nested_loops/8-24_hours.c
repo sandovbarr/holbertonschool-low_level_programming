da9c26e55b82e95e31b52bf842434a43c796dc5d
sandovbarr <sandovbarr@gmail.com>
2020-02-11 19:49:02 +0000

    8fixfinal

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,24 +7,18 @@
 
 void jack_bauer(void)
 {
-	int h, h2, m, m2;
+	int h, m;
 
-	for (h = 0; h <= 2; h++)
+	for (h = 0; h <= 23; h++)
 	{
-		for (h2 = 0; h2 <= 3; h2++)
+		for (m = 0; m <= 59; m++)
 		{
-			for (m = 0; m <= 5; m++)
-			{
-				for (m2 = 0; m2 <= 9; m2++)
-				{
-					_putchar(h + '0');
-					_putchar(h2 + '0');
-					_putchar(':');
-					_putchar(m + '0');
-					_putchar(m2 + '0');
-					_putchar('\n');
-				}
-			}
+			_putchar(h / 10 + 48);
+			_putchar(h % 10 + 48);
+			_putchar(':');
+			_putchar(m / 10 + 48);
+			_putchar(m % 10 + 48);
+			_putchar('\n');
 		}
 	}
 }
