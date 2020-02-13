57bd94bfa9fd0dc394142f572b1b823fe9bf9df8
sandovbarr <sandovbarr@gmail.com>
2020-02-13 22:07:35 +0000

    fixbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,22 +13,25 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if ((n % 3 == 0) && (n % 5 == 0))
+
+		if (n != 1)
 		{
-			printf("FizzBuzz ");
+			printf(" ");
 		}
+
 		if (n % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
-		else if (n % 5 == 0)
+		if (n % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
-		else
+		if (!(n % 3 == 0) && !(n % 5 == 0))
 		{
-			printf("%i ", n);
+			printf("%i", n);
 		}
+
 	}
 	printf("\n");
 	return (0);
