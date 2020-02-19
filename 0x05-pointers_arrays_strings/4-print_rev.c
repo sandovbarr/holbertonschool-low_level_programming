c1d81c94e31dd5d16724187743750f51e4dde7cf
sandovbarr <sandovbarr@gmail.com>
2020-02-19 23:48:12 +0000

    arreglo5

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,17 +7,18 @@
  */
 
 void print_rev(char *s)
+
 {
 
-	int tam, i;
+	int i, j;
 
 	for (i = 0; *(s + i) != '\0' ; i++)
 	{
 	}
 
-	for  (tam = i; tam >= 0 ; tam--)
+	for  (j = i; j >= 0 ; j--)
 	{
-		_putchar(*(s + tam));
+		_putchar(*(s + j));
 	}
 	_putchar('\n');
 }
