3447311e08448cd3f1022b548be988561a38a095
sandovbarr <sandovbarr@gmail.com>
2020-02-19 01:05:46 +0000

    7firstup

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * puts_half - print the second half of string.
+ * @str: string that comes for main function
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+
+
+	int l, i, midh;
+
+	for (l = 0; *(str + l) != '\0'; l++)
+	{
+	}
+
+	midh = l / 2;
+
+	for (i = midh; i <= l; i++)
+	{
+		_putchar(*(str + i));
+	}
+_putchar('\n');
+}
