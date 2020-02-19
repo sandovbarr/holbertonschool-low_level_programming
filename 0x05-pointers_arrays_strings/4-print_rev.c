bf91d6ab4648561cbcf8ea697b195f7d0efbfd9f
sandovbarr <sandovbarr@gmail.com>
2020-02-19 23:35:49 +0000

    fix4git

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,43 +1,22 @@
 #include "holberton.h"
 
 /**
- * _strlen - count the index of a string
- * @s: the string wich has beem recived
- * Return: count.
- */
-
-
-int _strlen(char *s)
-{
-
-	char *string;
-	int count = 0;
-
-	string = s;
-
-	while (*string != '\0') /*mientras que mi apuntador difiera de "\0" */
-	{
-		count++;  /*acá aumenta mi contador*/
-		string++; /*acá aumenta mi posicion del apuntador*/
-	}
-
-	return (count);
-
-}
-
-
-/**
  * print_rev - prints a string, followed by a new line, in reverse
  * @s: the string wich has beem recived
  * Return: none.
-[<65;38;29M */
+ */
 
 void print_rev(char *s)
 {
 
-	int tam = (_strlen(s) - 1);
-
+	int tam, i;
 	char *p;
+
+	for (i = 0; *(s + i) != '\0' ; i++)
+	{
+	}
+
+	tam = i;
 
 	p = s;
 
