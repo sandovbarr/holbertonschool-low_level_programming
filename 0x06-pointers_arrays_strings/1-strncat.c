7d06f9c8cc7504cb607030d370c0930fb56cc51b
sandovbarr <sandovbarr@gmail.com>
2020-02-20 17:28:52 +0000

    fix1onemore

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,7 +15,7 @@ char *_strncat(char *dest, char *src, int n)
 	{
 	}
 
-	for (j = 0 ; *(src + j) < *(src + n) ; j++, i++)
+	for (j = 0 ; (j < n) && (*(src + j) != '\0'); j++, i++)
 	{
 		*(dest + i) = *(src + j);
 	}
