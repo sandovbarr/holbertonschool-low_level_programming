ac955a9d5869f6b0326e6484ba2954b748cc148a
sandovbarr <sandovbarr@gmail.com>
2020-02-20 23:17:40 +0000

    4task

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverse string.
+ * @a: integer
+ * @n: integer
+ * Return: Always 0.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j;
+
+	for (i = 0; i <= n; i++)
+	{
+		n--;
+		j = *(a + n);
+		*(a + n) = *(a + i);
+		*(a + i) = j;
+	}
+
+}
