a2ebf070d2a62c1e84683689bafa9ab2f1e47346
sandovbarr <sandovbarr@gmail.com>
2020-02-24 22:04:28 +0000

    5searchstring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+/**
+ * _strstr - gets addres of string into another string
+ * @haystack: string to search
+ * @needle: string to search
+ * Return: pointer.
+ */
+
+char *_strstr(char *haystack, char *needle)
+
+{
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; haystack[i]; i++)
+	{
+		while (haystack[i] != needle[j])
+		{
+			haystack++;
+		}
+
+		while (haystack[i] == needle[j] && haystack[i] && needle[j])
+		{
+			i++;
+			j++;
+
+			if (needle[j] != '\0')
+			{
+				return (haystack++);
+			}
+		}
+	}
+	return (0);
+}
