2d4131c6bece51193a264efc6ed3fa29619313a0
sandovbarr <sandovbarr@gmail.com>
2020-02-24 19:59:19 +0000

    4searchfirstcoincidence

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+/**
+ * _strpbrk - returs firs coincidence
+ * @s: string to validate
+ * @accept: string that compares
+ * Return: Always 0.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+	return (0);
+}
