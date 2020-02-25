0bef368e1f19574af9d18510f99652d4e8b5eef5
sandovbarr <sandovbarr@gmail.com>
2020-02-25 20:22:25 +0000

    set_string

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+/**
+ * set_string - check the code for Holberton School students.
+ * @s: double pointer to address S1
+ * @to: pointer to s0
+ * Return: Always 0.
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
