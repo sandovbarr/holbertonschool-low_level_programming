2c07fea01e71ca8b576a7df44b2d6a9305d32187
sandovbarr <sandovbarr@gmail.com>
2020-02-26 22:33:09 +0000

    6prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+/**
+ * operation - check validates n
+ * if it is prime
+ * @n: integer
+ * Return: Always 0.
+ */
+
+int operation (int n, int x)
+{
+	if (n <= 1)
+		return (0);
+
+	if (n == x)
+		return (1);
+
+	if (n % x == 0)
+		return (0);
+
+	return (operation (n, x + 1));
+
+}
+
+/**
+ * is_prime_number - check validates n
+ * if it is prime
+ * @n: integer
+ * Return: Always 0.
+ */
+int is_prime_number(int n)
+{
+	return (operation(n, 2));
+}
