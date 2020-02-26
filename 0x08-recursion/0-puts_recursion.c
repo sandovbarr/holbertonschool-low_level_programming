31f545f6dd225b38188c434c1a6c0255c454c569
sandovbarr <sandovbarr@gmail.com>
2020-02-26 17:27:20 +0000

    fix0v2

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,9 +8,9 @@
 
 void _puts_recursion(char *s)
 {
-        _putchar(*s);
-        if (*s != '\0')
-                _puts_recursion(s + 1);
-        if (*s == '\0')
-                _putchar('\n');
+	_putchar(*s);
+	if (*s != '\0')
+		_puts_recursion(s + 1);
+	if (*s == '\0')
+		_putchar('\n');
 }
