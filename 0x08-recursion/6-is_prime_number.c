dd3cb0bdaeaf12ed3f9d371c03b25b00e860f26b
sandovbarr <sandovbarr@gmail.com>
2020-02-26 22:35:01 +0000

    fixprimenumber

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,10 +2,11 @@
  * operation - check validates n
  * if it is prime
  * @n: integer
+ * @x: integer
  * Return: Always 0.
  */
 
-int operation (int n, int x)
+int operation(int n, int x)
 {
 	if (n <= 1)
 		return (0);
@@ -16,7 +17,7 @@ int operation (int n, int x)
 	if (n % x == 0)
 		return (0);
 
-	return (operation (n, x + 1));
+	return (operation(n, x + 1));
 
 }
 
