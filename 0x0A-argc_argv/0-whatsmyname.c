1cf6fcdd684420ed40b97ab3d0fb798765a812e5
fernandoSandoval <sandovbarr@gmail.com>
2020-02-29 20:15:02 -0500

    changethename

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *main - function with arguments
+ *@argc: cant of arguments
+ *@argv: value of arguments
+ *Return: integer
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)(argc);
+	printf("%s\n",*argv);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+now is time to know arguments for main function
+lets know it!
