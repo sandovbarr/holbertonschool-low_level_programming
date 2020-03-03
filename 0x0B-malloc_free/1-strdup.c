ddfeae19576bf68db46b00a9739a95b1ad2991dd
S3RG1O1994 <se.diaz60@gmail.com>
2020-03-03 18:55:19 +0000

    fix1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,22 +11,24 @@
 
 char *_strdup(char *str)
 {
-	int i, j;
-	char *p;
+        int i, j;
+        char *p;
 
-	for (i = 0; str[i]; i++)
-	{
-	}
+        for (i = 0; str[i]; i++)
+        {
+        }
 
-	p = malloc(sizeof(char) * i);
+        i++;
 
-	if (p == NULL)
-		return (NULL);
+        p = malloc(sizeof(char) * i);
 
-	for (j = 0; j < i; j++)
-	{
-		p[j] = str[j];
-	}
+        if (p == NULL)
+                return (NULL);
 
-	return (p);
+        for (j = 0; j < i; j++)
+        {
+                p[j] = str[j];
+        }
+
+        return (p);
 }
