881b018b9c579b15b42b38a29d6e65cdb23faea8
Fernando Sandoval <sandovbarr@gmail.com>
2020-03-09 20:14:29 +0000

    typestructure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - the first structure of dog
+ * @name: member name
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * description: first structure
+ */
+
+typedef struct dog
+{
+
+	char *name;
+	float age;
+	char *owner;
+
+} dog;
+
+#endif
---
+++ 0x0E-structures_typedef/holberton.h
@@ -0,0 +1,6 @@
+# ifndef HOLBERTON_FILE
+# define  HOLBERTON_FILE
+
+int _putchar(char c);
+
+# endif
