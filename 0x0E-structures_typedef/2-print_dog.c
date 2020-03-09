7a5b5c197b4ba58ab8d2aece909cd7cfda0db408
Fernando Sandoval <sandovbarr@gmail.com>
2020-03-09 21:12:29 +0000

    printstructure

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints the content of a structur
+ * @d: pointer to structure
+ * Return: Always 0.
+ */
+
+void print_dog(struct dog *d)
+{
+
+	if (!d)
+		return;
+
+	if (d->name == NULL)
+		printf("name: (nil)\n");
+	else
+		printf("name: %s\n", d->name);
+
+	if (d->age == 0)
+		printf("age: (nil)\n");
+	else
+		printf("age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("owner: (nil)\n");
+	else
+	printf("owner: %s\n", d->owner);
+}
