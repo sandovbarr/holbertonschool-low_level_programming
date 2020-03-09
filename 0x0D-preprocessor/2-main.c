1b51506aa5b86d974cea0b981a3cf902cd8b72a0
Fernando Sandoval <sandovbarr@gmail.com>
2020-03-09 17:07:14 +0000

    upload2task

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints the name of the complied
+ * compiled file
+ * return: zero
+ */
+
+int main(void)
+{
+
+	printf("%s\n", __FILE__);
+	return (0);
+
+}
