2533bc12e4271290e48e0c0bbd060b21233035b9
Fernando Sandoval <sandovbarr@gmail.com>
2020-03-09 23:00:51 +0000

    4taskup

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,54 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - function that creates a new dog.
+ * @name: pointer to structure.name
+ * @age: age of the new dog
+ * @owner: pointer to new owner
+ * Return: pointer to new data
+ */
+
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *pointer;
+	char *namecopy, *ownercopy;
+	int i, j, m, n;
+
+	for (i = 0; name[i]; i++)
+	{}
+	for (j = 0; owner[j]; j++)
+	{}
+
+	namecopy = malloc(sizeof(char) * i + 1);
+	if (namecopy == NULL)
+		return (NULL);
+
+	ownercopy = malloc(sizeof(char) * j + 1);
+	if (ownercopy == NULL)
+	{
+		free(namecopy);
+		return (NULL);
+	}
+
+	for (m = 0; m <= i; m++)
+		namecopy[m] = name[m];
+
+	for (n = 0; n <= j; n++)
+		ownercopy[n] = owner[n];
+
+	pointer = malloc(sizeof(dog_t));
+	if (pointer == NULL)
+	{
+		free(namecopy);
+		free(ownercopy);
+		return (NULL);
+	}
+
+	pointer->name = namecopy;
+	pointer->age = age;
+	pointer->owner = ownercopy;
+	return (pointer);
+}
