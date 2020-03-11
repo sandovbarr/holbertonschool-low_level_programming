a0b998b2d11201780cf26e4b703dcefe4fdf86b9
Fernando Sandoval <sandovbarr@gmail.com>
2020-03-11 17:46:47 +0000

    function_prinsts

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_name - function that put the pointer name into
+ * a parameter char * of function voidn f
+ * @name: this name comes from other main
+ * @f: is a pointer to function taht prints
+ * Return: no return;
+ */
+
+void print_name(char *name, void (*f)(char *));
+{
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/holberton.h
@@ -0,0 +1,7 @@
+# ifndef HOLBERTON_FILE
+# define  HOLBERTON_FILE
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+# endif
