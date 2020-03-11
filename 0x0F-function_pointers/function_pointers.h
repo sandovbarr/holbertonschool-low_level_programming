89cb02cc89a175b9a8a779a3e9e7624c85580bed
Fernando Sandoval <sandovbarr@gmail.com>
2020-03-11 19:28:49 +0000

    [add] file 2 task

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,8 @@
-# ifndef HOLBERTON_FILE
-# define  HOLBERTON_FILE
+# ifndef _FUNCTION_POINTERS_H_
+# define _FUNCTION_POINTERS_H_
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 # endif
