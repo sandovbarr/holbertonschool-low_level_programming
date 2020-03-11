7035418aee1ef46a9905e8ac45f280012fa073cd
Fernando Sandoval <sandovbarr@gmail.com>
2020-03-11 23:38:57 +0000

    [ADD]3task

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+/**
+ * op_add - function that adds two numbers
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * op_sub - function that subtracts two numbers
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+
+/**
+ * op_mul - function that multiplicate two numbers
+ * @a: integer
+ * @b: intger
+ * Return: an int
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - function that divides two numbers
+ * @a: integer
+ * @b: integer
+ * Return: an int
+ */
+
+int op_div(int a, int b)
+{
+	return  (a / b);
+}
+
+
+
+/**
+ * op_mod - function that calculate the module of a
+ * with b
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
