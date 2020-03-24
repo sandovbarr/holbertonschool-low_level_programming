1ea4b1f3412e7950aef8b77b36af42e648ec15a6
Fernando Sandoval <sandovbarr@gmail.com>
2020-03-24 15:24:44 -0500

    [FIX] conditional

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *scooch, *newnode, *filter;
 	unsigned int counter = 0, counter2 = 0;
 
-	if (!*head)
+	if (!*head || !idx)
 		return (NULL);
 	if (idx == 0)
 	{
