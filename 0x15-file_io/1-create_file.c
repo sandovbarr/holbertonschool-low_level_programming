bb2373d46b507751fcbf64f44c13d1d334928ba2
Fernando Sandoval <sandovbarr@gmail.com>
2020-03-31 15:54:33 -0500

    [FIX]whileinsidecondition

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,11 +20,10 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	while (text_content[cs])
-		cs++;
-
 	if (text_content)
 	{
+		while (text_content[cs])
+			cs++;
 		fdescpdwrite = write(fd, text_content, cs);
 		if (fdescpdwrite == -1)
 			return (-1);
