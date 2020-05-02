0564914bca569b10d10d7ec6e2e2ecff3640d1d5
Fernando Sandoval <sandovbarr@gmail.com>
2020-05-02 13:08:30 -0500

    [FIX]conditionalnoindex

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,6 +28,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		newnode->next = *h;
+		aux->prev = newnode;
 		*h = newnode;
 		return (newnode);
 	}
