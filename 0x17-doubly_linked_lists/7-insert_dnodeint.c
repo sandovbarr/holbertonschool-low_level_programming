725eaedc0b460c68ddf5b180c426646ab9c49825
Fernando Sandoval <sandovbarr@gmail.com>
2020-05-02 11:27:49 -0500

    [FIX]contidionalheadull

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,6 +16,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *newnode, *aux = *h;
 	unsigned int iter = 0;
 
+	if (!*h)
+		return (NULL);
 	newnode = malloc(sizeof(dlistint_t));
 	if (!newnode)
 		return (NULL);
