c21486d0f25fc00afe1e2e6bbc9f3701b4c037bf
Fernando Sandoval <sandovbarr@gmail.com>
2020-05-02 17:19:24 -0500

    [FIX]nolastnode

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,7 +22,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	newnode->n = n, newnode->next = NULL, newnode->prev = NULL;
 
 	if (!*h)
-		return (free(newnode), NULL);
+	{
+		*h = newnode;
+		return (newnode);
+	}
 	if (idx == 0)
 	{
 		newnode->next = *h;
