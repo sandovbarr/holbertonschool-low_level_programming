a08048edbb929971d144c88c9d8eee590e85e581
Fernando Sandoval <sandovbarr@gmail.com>
2020-05-02 19:12:52 -0500

    [FIX]Conditionforlastin

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -24,7 +24,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	while (aux)
 	{
-		if (!aux->next)
+		if (!aux->next && iter == index)
 		{
 			aux2 = aux->prev;
 			aux2->next = NULL;
