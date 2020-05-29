8d51ef5960faa3932d6388bd2b88e6b40ec8c25b
fernando <sandovbarr@gmail.com>
2020-05-29 17:52:23 -0500

    [ADD]feature

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -20,8 +20,8 @@ void hash_table_delete(hash_table_t *ht)
 		while (nodes)
 		{
 			nodeshanlder = nodes->next;
-			free(nodes->value);
 			free(nodes->key);
+			free(nodes->value);
 			free(nodes);
 			nodes = nodeshanlder;
 		}
