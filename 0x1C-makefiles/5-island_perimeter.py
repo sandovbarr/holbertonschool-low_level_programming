28c51b597d608a05f833aa96f1c402a2138707db
Fernando Sandoval <sandovbarr@gmail.com>
2020-07-03 21:34:12 -0500

    [ADD] new features, fix edge cases

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,17 +8,22 @@
 
 def island_perimeter(grid):
     ''' computes perimeter of island grid given '''
-    cellvalue = 0
+    cellvalue = 4
+    tot = 0
 
     for row in range(len(grid)):
         for col in range(len(grid[row])):
             if grid[row][col] == 1:
-                if grid[row + 1][col] == 0:
-                    cellvalue += 1
-                if grid[row - 1][col] == 0:
-                    cellvalue += 1
-                if grid[row][col + 1] == 0:
-                    cellvalue += 1
-                if grid[row][col - 1] == 0:
-                    cellvalue += 1
-    return(cellvalue)
+                if row < len(grid) - 1:
+                    if grid[row + 1][col] == 1:
+                        cellvalue -= 1
+                    if grid[row - 1][col] == 1:
+                        cellvalue -= 1
+                if col < len(grid[row]) - 1:
+                    if grid[row][col + 1] == 1:
+                        cellvalue -= 1
+                    if grid[row][col - 1] == 1:
+                        cellvalue -= 1
+                tot += cellvalue
+                cellvalue = 4
+    return(tot)
