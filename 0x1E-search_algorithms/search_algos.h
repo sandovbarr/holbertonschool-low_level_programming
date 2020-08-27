c9115c410db6634f15513119ddece42f304890ae
fernandoSandoval <sandovbarr@gmail.com>
2020-08-27 16:22:51 -0500

    [ADD] header new prototype

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,10 +4,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <unistd.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 void print_array(int *array, size_t min, size_t max);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif
